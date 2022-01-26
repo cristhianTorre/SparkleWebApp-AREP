f6d1923993683e4ad8a78e3417563cd523acfafd
unknown <cristhian.torres@labinfo.is.escuelaing.edu.co>
2022-01-26 15:13:22 -0500

    Proyecto completo.

--- src/main/java/edu/escuelaing/arem/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -8,6 +8,15 @@ import static spark.Spark.*;
 public class SparkWebApp {
 
     public static void main(String[] args) {
+        System.out.println("Initiating server... Ready");
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080; //returns default port if heroku-port isn't set
     }
 }
